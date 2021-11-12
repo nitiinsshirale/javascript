85fee5a45866679fc1d960ed5d36c932427dcf31
techmomo <mkhursheed@spireon.com>
2021-11-12 12:59:27 +0530

    formatting

--- react/hello-react/src/components/Employee.jsx
+++ react/hello-react/src/components/Employee.jsx
@@ -3,9 +3,7 @@ const Employee = (props)=>{
     return <>
     <p><strong>Welcome</strong>, to Employee Component</p>
     <h3>Message : {props.message}</h3>
-    {
-        console.log(props.userVo)
-    }
+    {console.log(props.userVo)}
     </>
 }
 
