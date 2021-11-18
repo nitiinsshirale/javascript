f911095d03ac0508d227bface0aec85bef73795f
techmomo <mkhursheed@spireon.com>
2021-11-18 12:58:25 +0530

    update the handlers

--- react/hello-react/src/components/Project.jsx
+++ react/hello-react/src/components/Project.jsx
@@ -9,10 +9,19 @@ const Project = (props) =>{
     const [project, setProject] = useState({});
 
     const handleChange = (e) =>{
+        console.log(e);
         // in case we want to update the existing state 
         setProject({...project, [e.target.name]: e.target.value});
         // in case we want to over write the existing state
         // setProject({[e.target.name]: e.target.value});
+    }
+
+    const handleName = e=>{
+        setProject({...project, "name": e.target.value});
+    }
+
+    const handleBudget = e=>{
+        setProject({...project, "budget": e.target.value});
     }
 
     const handleSubmit = (e)=>{
@@ -30,11 +39,11 @@ const Project = (props) =>{
         <form onSubmit={handleSubmit}>
             <p>
                 <label>Name:</label>
-                <input type="text" name="name" id="name" onChange={handleChange}/>
+                <input type="text" name="name" id="name" onChange={handleName}/>
             </p>
             <p>
                 <label>Budget:</label>
-                <input type="number" name="budget" id="budget" onChange={handleChange}/>
+                <input type="number" name="budget" id="budget" onChange={handleBudget}/>
             </p>
             {/* { project.name !== '' && project.budget !== 0 ? <p>Name : {project.name} , Budget : {project.budget}</p>:'' } */}
             <button type="submit">Submit</button>
